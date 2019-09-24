4a3dd144258d5bb6b8b9f85da3aedeb9bd451755
conoroshea1996 <conoshea12@gmail.com>
2019-09-24 20:46:08 +0100

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="nav-wrapper blue darken-1">
+                <Container maxWidth='md'>
+                    <a href="#" className="brand-logo">Logo</a>
+                    <ul id="nav-mobile" className="right ">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </Container>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;

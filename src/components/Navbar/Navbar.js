24cbe4b0ce6c06ce6858ef9565204a7c4c336f26
conoroshea1996 <conoshea12@gmail.com>
2019-09-24 20:58:38 +0100

    update path for home to be exact path

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
                 <Container maxWidth='md'>
                     <a href="#" className="brand-logo">Logo</a>
                     <ul id="nav-mobile" className="right ">
-                        <li><a href="/home">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/contact">Contact</a></li>
                     </ul>

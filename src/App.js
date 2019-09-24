8809cc826292085dc7bfea8041ce5b9d53997afb
conoroshea1996 <conoshea12@gmail.com>
2019-09-24 20:31:56 +0100

    update component file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (

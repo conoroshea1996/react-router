b41c4c8ada853b4dc591a0a158cb16f0bf125566
conoroshea1996 <conoshea12@gmail.com>
2019-09-26 21:10:24 +0100

    create post component

---
+++ src/components/Post/Post.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Container } from '@material-ui/core';
+
+class Post extends Component {
+    state = {
+        id: null
+    }
+    componentDidMount() {
+        let id = this.props.match.params.post_id
+
+        this.setState({
+            id
+        })
+    }
+    render() {
+        console.log(this.state);
+        return (<Container maxWidth='md'>
+            <h4>route parm</h4>
+        </Container>
+        )
+    }
+}
+
+export default Post;
+

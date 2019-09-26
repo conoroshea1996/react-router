2c490d1fcd06c2439fd835af8fbda223caf7354c
conoroshea1996 <conoshea12@gmail.com>
2019-09-26 21:54:38 +0100

    post now display correct content

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import { Container } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 
 class Post extends Component {
     state = {
-        id: null
+        post: ''
     }
     componentDidMount() {
         let id = this.props.match.params.post_id
-        this.setState({
-            id
-        })
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => {
+                this.setState({
+                    post: res.data
+                })
+            })
     }
 
     render() {
-        console.log(this.state);
-        return (<Container maxWidth='md'>
-            <h4>route parm</h4>
-        </Container>
+        return (
+            <Container maxWidth='md'>
+                <Card style={{ marginTop: '1rem' }} >
+                    <CardContent style={{ textAlign: 'center' }} >
+                        <Typography component="h2" gutterBottom>
+                            {this.state.post.title}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                            {this.state.post.body}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                            {this.state.post.id}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Container>
         )
     }
 }
